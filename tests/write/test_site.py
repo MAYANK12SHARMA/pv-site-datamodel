8b9ef507078ed8583735984695375f650992017a
MAYANKSHARMA <sharmaji123mayank@gmail.com>
2025-03-11 22:39:12 +0530

    Remove unnecessary owner assertion in site creation test

--- tests/write/test_site.py
+++ tests/write/test_site.py
@@ -102,9 +102,7 @@ def test_create_new_site_in_specified_country(db_session):
         message == f"Site with client site id {site.client_site_id} "
         f"and site uuid {site.site_uuid} created successfully"
     )
-    # owner was not provided so it should be None
-    assert site.owner is None
-
+   
 
 def test_edit_site(db_session):
     """Test the update of site metadata"""
